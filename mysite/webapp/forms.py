e94dc2e3b4c0d19565e202e7f7c87a9a0155ab14
Eyad Hasan <ehasan3@gmu.edu>
2019-04-17 18:42:15 -0400

    i broke all the CSS and forms sorry

--- mysite/webapp/forms.py
+++ mysite/webapp/forms.py
@@ -1,7 +1,23 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit
+from crispy_forms.bootstrap import StrictButton
 
 class zipform(forms.Form):
 	zipcode = forms.CharField(max_length = 5)
+	def __init__(self, *args, **kwargs):
+		super(zipform, self).__init__(*args, **kwargs)
+		self.helper = FormHelper()
+		self.helper.form_id = "zipCodeSubmit"
+		self.helper.form_class = "form-horizontal"
+		self.label_class = 'col-lg-2'
+		self.field_class = 'col-lg-8'
+		self.helper.layout = Layout(
+			'Zipcode',
+			StrictButton('Submit', css_class='btn-default'),
+		)
+		
+		#self.helper.add_input(Submit('submit', 'Submit'))
 
 	def clean(self):
 		cleaned_data = super(zipform,self).clean()
