a64a880aad5338beb87a313babe76e7a3603093c
danieltmc <me@danieltmc.com>
2019-04-25 17:00:07 -0400

    Revised run script

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
 import subprocess
+import webbrowser
 
-subprocess.run("pipenv run python3 mysite/manage.py runserver", shell=True, check=True)
+bash = "pipenv run python3 mysite/manage.py runserver"
+process = subprocess.Popen(bash.split(), stdout=subprocess.PIPE)
+webbrowser.open("http://127.0.0.1:8000/webapp/home", new = 1)
+output, error = process.communicate()
