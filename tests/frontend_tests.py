72374fd9384122827fe97826bbc86ebc43a87f0d
danieltmc <me@danieltmc.com>
2019-04-26 11:48:01 -0400

    Frontend tests corrected - 5/5 work now

--- tests/frontend_tests.py
+++ tests/frontend_tests.py
@@ -1,27 +1,60 @@
+#!/usr/bin/env python3
 from selenium import webdriver
 import unittest
 
+# These tests all require that you have Chrome installed.
+# If you use a different browser, try using these instead:
+# webdriver.Firefox() or webdriver.InternetExplorer()
 class GetWebpage(unittest.TestCase):
 	def setUp(self):
 		self.driver = webdriver.Chrome()
-		self.driver.maximize_window()
 		self.driver.get("http://127.0.0.1:8000/webapp/home")
 	def test_getpage(self):
-		self.assertEqual(self.driver.title, "Sunny With a Chance of Bops")
+		assert self.driver.title == "Sunny With a Chance of Bops"
 	def tearDown(self):
 		self.driver.quit()
 
-class InputZIP(unittest.TestCase):
+class CheckSwitchButton_Playlist(unittest.TestCase):
 	def setUp(self):
 		self.driver = webdriver.Chrome()
-		self.driver.maximize_window()
 		self.driver.get("http://127.0.0.1:8000/webapp/home")
-	def test_putzip(self):
-		#element = driver.find_element_by_id("zipcode")
+	def image_search(self):
+		element = self.driver.find_element_by_class_name("btn btn-secondary")
+		element.submit()
+		assert "Switch to: playlists" in self.driver.page_source
+	def tearDown(self):
+		self.driver.quit()
+
+class CheckSwitchButton_Song(unittest.TestCase):
+	def setUp(self):
+		self.driver = webdriver.Chrome()
+		self.driver.get("http://127.0.0.1:8000/webapp/homeSong")
+	def image_search(self):
+		element = self.driver.find_element_by_class_name("btn btn-secondary")
+		element.submit()
+		assert "Songs" in self.driver.page_source
+	def tearDown(self):
+		self.driver.quit()
+
+class InputZIP_Playlist(unittest.TestCase):
+	def setUp(self):
+		self.driver = webdriver.Chrome()
+		self.driver.get("http://127.0.0.1:8000/webapp/home")
+	def test_zip(self):
 		element = self.driver.find_element_by_name("zipcode")
 		element.send_keys("22030")
-		#element = self.driver.find_element_by_class_name("btn btn-dark")
-		#element.click()
+		element.submit()
+		assert "https://open.spotify.com/embed" in self.driver.page_source
+	def tearDown(self):
+		self.driver.quit()
+
+class InputZIP_Song(unittest.TestCase):
+	def setUp(self):
+		self.driver = webdriver.Chrome()
+		self.driver.get("http://127.0.0.1:8000/webapp/homeSong")
+	def test_zip(self):
+		element = self.driver.find_element_by_name("zipcode")
+		element.send_keys("22030")
 		element.submit()
 		assert "https://open.spotify.com/embed" in self.driver.page_source
 	def tearDown(self):
